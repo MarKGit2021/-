04d8d95ff4f766d4783b6a12eefbce7a9457068c
MarKGit2021 <kuranovamarie2018@gmail.com>
2021-12-04 15:55:01 +0300

    Желтые окружности

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt
+
+
+class MyWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.flag = False
+
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.flag = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+            self.flag = False
+
+    def draw(self, qp):
+        qp.setPen(QPen(Qt.yellow, 10, Qt.SolidLine))
+        for i in range(random.randint(1, 10)):
+            rad = random.randint(10, 100)
+            cor_X = random.randint(0, 740)
+            cor_Y = random.randint(0, 510)
+            qp.drawEllipse(cor_X, cor_Y, rad, rad, )
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWin()
+    ex.show()
+    sys.exit(app.exec())
